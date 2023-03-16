6941cb46408dde0b12ffaa9f6b5aed505da5eb3e
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-16 16:26:11 +0200

    Add project files.

---
+++ DependecyInversionRealExample/ChessEngine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ChessDependencyInversion
+{
+    public class ChessEngine
+    {
+        public void Start()
+        {
+            while (true)
+            {
+                Console.WriteLine("Drawing board");
+                Thread.Sleep(1000);
+
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ DependecyInversionRealExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DependecyInversionRealExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
